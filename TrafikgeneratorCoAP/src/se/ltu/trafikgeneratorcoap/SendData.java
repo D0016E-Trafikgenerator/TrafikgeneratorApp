f737576d82cebaf9f21bbd5c507f21a962a6ca5a
Stefan <jonsen.stefan@gmail.com>
2014-02-25 11:22:46 +0100

    Added functionality to read config file and change it depending on user input

--- TrafikgeneratorCoAP/src/se/ltu/trafikgeneratorcoap/SendData.java
+++ TrafikgeneratorCoAP/src/se/ltu/trafikgeneratorcoap/SendData.java
@@ -2,6 +2,7 @@ package se.ltu.trafikgeneratorcoap;
 
 import se.ltu.trafikgeneratorcoap.send.Sending;
 import se.ltu.trafikgeneratorcoap.send.TrafficConfig;
+import se.ltu.trafikgeneratorcoap.send.Settings;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.ProgressDialog;
@@ -12,16 +13,16 @@ public class SendData extends AbstractActivity {
     private ProgressDialog progressDialog;  
   
 	private String ip;
-	private int port;
-	private int seconds;
+	private Integer port;
+	private Integer seconds;
 	private String filePath = "";
-	private int timeout;
-	private float random;
-	private int retransmitt;
-	private int nStart;
-	private float probingRate;
-	private int payloadSize;
-	private int connections;
+	private Integer timeout;
+	private Float random;
+	private Integer retransmitt;
+	private Integer nStart;
+	private Float probingRate;
+	private Integer payloadSize;
+	private Integer connections;
 	
 	private Intent intent;
 	
@@ -29,27 +30,46 @@ public class SendData extends AbstractActivity {
     public int indexer = 0;
     private int progressbarUpdate = 1;
     
-    private final TrafficConfig config = new TrafficConfig(TrafficConfig.fileToString(filePath));
+    private TrafficConfig config;
     
     /** Called when the activity is first created. */  
     @Override  
     public void onCreate(Bundle savedInstanceState)  
     {  
         super.onCreate(savedInstanceState);  
-  
         intent = getIntent();
         timeout = parseInt("timeout");
-        random = parseFloat("random");
         retransmitt = parseInt("retransmitt");
         nStart = parseInt("nstart");
-	    probingRate = parseFloat("probingrate");
 	    payloadSize = parseInt("payloadsize");
 	    port = parseInt("port");
 	    seconds = parseInt("time");
 	    connections = parseInt("connections");
-	    
+        random = parseFloat("random");
+	    probingRate = parseFloat("probingrate");
 	    filePath = intent.getStringExtra("filename");
 	    ip = intent.getStringExtra("ip");
+	    
+	    config = new TrafficConfig(TrafficConfig.fileToString(filePath));
+	    
+	    if(!(timeout == null))
+	    	config.setIntegerSetting(Settings.COAP_ACK_TIMEOUT, timeout);
+	    if(!(retransmitt == null))
+	    	config.setIntegerSetting(Settings.COAP_MAX_RETRANSMIT, retransmitt);
+	    if(!(nStart == null))
+	    	config.setIntegerSetting(Settings.COAP_NSTART, nStart);
+	    if(!(payloadSize == null))
+	    	config.setIntegerSetting(Settings.TRAFFIC_MESSAGESIZE, payloadSize);
+	    if(!(port == null))
+	    	config.setIntegerSetting(Settings.TEST_TESTPORT, port);
+	    if(!(seconds == null))
+	    	config.setIntegerSetting(Settings.TRAFFIC_MAXSENDTIME, seconds);	    
+	    if(!(random == null))
+	    	config.setDecimalSetting(Settings.COAP_ACK_RANDOM_FACTOR, random);
+	    if(!(probingRate == null))
+	    	config.setDecimalSetting(Settings.COAP_PROBING_RATE, probingRate);
+	    
+	    config.setStringSetting(Settings.TEST_SERVER, ip);
 	    
         //Initialize a LoadViewTask object and call the execute() method 
 	    for(int i = 0; i < connections; i++)
@@ -59,23 +79,23 @@ public class SendData extends AbstractActivity {
 	    }
     } 
     
-    private float parseFloat(String s)
+    private Float parseFloat(String s)
     {
 	    try {
 			float floatReturn = Float.parseFloat(intent.getStringExtra(s));
 			return floatReturn;
 		} catch (NumberFormatException e) {
-			return 0.0f;
+			return null;
 		}
     }
     
-    private int parseInt(String s)
+    private Integer parseInt(String s)
     {
 	    try {
 			int intReturn = Integer.parseInt(intent.getStringExtra(s));
 			return intReturn;
 		} catch (NumberFormatException e) {
-			return 0;
+			return null;
 		}
     }
     
@@ -109,7 +129,7 @@ public class SendData extends AbstractActivity {
         {   
         	Sending.sendData(filePath, getApplicationContext());
         	publishProgress(progressbarUpdate++);
-        	return null;  
+        	return null;
         }  
   
         //Update the progress  
@@ -120,7 +140,6 @@ public class SendData extends AbstractActivity {
             progressDialog.setProgress(values[0]);  
         }  
   
-
         //after executing the code in the thread  
         @Override  
         protected void onPostExecute(Void result)  
