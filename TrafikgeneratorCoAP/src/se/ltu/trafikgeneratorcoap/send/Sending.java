7398e45aaa89e5f02f0468c093ad570a5d3bcb2a
davnym-4 <davnym-4@student.ltu.se>
2014-03-08 16:18:21 +0100

    Now it actually sets CoAP parameters. Oops!

--- TrafikgeneratorCoAP/src/se/ltu/trafikgeneratorcoap/send/Sending.java
+++ TrafikgeneratorCoAP/src/se/ltu/trafikgeneratorcoap/send/Sending.java
@@ -1,5 +1,7 @@
 package se.ltu.trafikgeneratorcoap.send;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -13,6 +15,7 @@ import ch.ethz.inf.vs.californium.coap.CoAP;
 import ch.ethz.inf.vs.californium.coap.Request;
 import ch.ethz.inf.vs.californium.coap.Response;
 import ch.ethz.inf.vs.californium.coap.CoAP.ResponseCode;
+import ch.ethz.inf.vs.californium.network.CoAPEndpoint;
 
 public class Sending {
 	private static int maxpacketsize = 1024;
@@ -21,14 +24,24 @@ public class Sending {
 	private static Random random = new Random();
 	private static int sentMessages = 0;
 	private static long lastSentMessage = 0L;
-	public static void sendData(TrafficConfig config, Context context) {
-		//Log.d("dummycoap", "");
+	public static void sendDataFromApp(TrafficConfig config, Context context) {
 		Sending.context = context;
+		sendData(config);
+	}
+	public static void sendDataFromPC(TrafficConfig config) {
+		sendData(config);
+	}
+	private static void sendData(TrafficConfig config) {
 		SntpClient internetTimeClient = new SntpClient();
 		SntpClient internetTimeClient2 = new SntpClient();
 		int numberOfTests = config.getIntegerSetting(Settings.TEST_REPEATS);
 		int timeBetweenTests = Math.round(config.getDecimalSetting(Settings.TEST_INTERMISSION));
 		String uri = config.getStringSetting(Settings.TEST_SERVER);
+		/*int controlPort = 5683;
+		if (uri.split(":").length == 2) {
+			controlPort = Integer.valueOf(uri.split(":")[1]);
+			uri = uri.split(":")[0];
+		}*/
 		int testport = config.getIntegerSetting(Settings.TEST_TESTPORT);
 		int payloadSize = config.getIntegerSetting(Settings.TRAFFIC_MESSAGESIZE);
 		if (payloadSize + headersize > maxpacketsize) payloadSize = maxpacketsize-headersize;
@@ -39,11 +52,23 @@ public class Sending {
 		String date = (new SimpleDateFormat("yyyyMMddHHmm", Locale.getDefault())).format(new Date());
 		CoAP.Type type = config.getStringSetting(Settings.COAP_MESSAGETYPE).equals("CON")?CoAP.Type.CON:CoAP.Type.NON;
 		//Test protocol 1.3a.2
+		
+		CoAPEndpoint controlEndpoint = new CoAPEndpoint();
+		try {
+			controlEndpoint.start();
+		} catch (IOException e) {;}
 		Request control = Request.newPost();
 		String controlPayload = TrafficConfig.networkConfigToStringList(config.toNetworkConfig());
-		control.setURI("coap://" + uri + "/control?time=" + date);
+		String controlURI = "coap://" + uri + "/control?time=" + date;
+		control.setURI(controlURI);
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
 		control.setPayload(controlPayload);
-		control.send();
+		control.send(controlEndpoint);
 		Response response;
 		try {
 			response = control.waitForResponse();
@@ -67,17 +92,19 @@ public class Sending {
 						"ms\npackets to send: at most " + Math.round((numberOfTests*seconds*1000)/((float)timeBetweenPackets)) +
 						"(or " + maxMessages + ")");
 				Thread.sleep(5000);
+				CoAPEndpoint dataEndpoint = new CoAPEndpoint(config.toNetworkConfig());
+				dataEndpoint.start();
 				if (config.getStringSetting(Settings.TRAFFIC_TYPE).equals("CONSTANT_SOURCE")) {
 					if (config.getStringSetting(Settings.TRAFFIC_MODE).equals("TIME")) {
-						Sending.runTimeTest(seconds, uri, testport, type, payloadSize, timeBetweenPackets, timeBetweenTests, numberOfTests);
+						Sending.runTimeTest(dataEndpoint, uri, testport, seconds, type, payloadSize, timeBetweenPackets, timeBetweenTests, numberOfTests);
 						testDone = true;
 					}
 					else if (config.getStringSetting(Settings.TRAFFIC_MODE).equals("MESSAGES")) {
-						Sending.runMessageTest(maxMessages, uri, testport, type, payloadSize, timeBetweenPackets, timeBetweenTests, numberOfTests);
+						Sending.runMessageTest(dataEndpoint, uri, testport, maxMessages, type, payloadSize, timeBetweenPackets, timeBetweenTests, numberOfTests);
 						testDone = true;
 					}
 					else if (config.getStringSetting(Settings.TRAFFIC_MODE).equals("FILETRANSFER")) {
-						Sending.runMessageTest(maxMessages, uri, testport, type, payloadSize, timeBetweenPackets, timeBetweenTests, numberOfTests);
+						Sending.runMessageTest(dataEndpoint, uri, testport, maxMessages, type, payloadSize, timeBetweenPackets, timeBetweenTests, numberOfTests);
 						testDone = true;
 					}
 				}
@@ -88,7 +115,7 @@ public class Sending {
 					//Test protocol 1.3a.6
 					control = Request.newDelete();
 					control.setURI("coap://" + uri + "/control?" + "token=" + token);
-					control.send();
+					control.send(controlEndpoint);
 					//Test protocol 1.3a.7
 					if (internetTimeClient2.requestTime(uri.split(":")[0], ntpPort, 1000))
 						internetTimeClient2.requestTime("pool.ntp.org", 123, 1000);
@@ -106,9 +133,9 @@ public class Sending {
 					}
 				}
 			}
-		} catch (InterruptedException e1) {;}
+		} catch (InterruptedException e1) {;} catch (IOException e) {;}
 	}
-	private static void runTimeTest(Integer seconds, String uri, Integer testport, CoAP.Type type, Integer payloadSize,
+	private static void runTimeTest(CoAPEndpoint endpoint, String uri, Integer testport, Integer seconds, CoAP.Type type, Integer payloadSize,
 			Integer timeBetweenPackets, Integer timeBetweenTests, Integer numberOfTests) {
 		try {
 			for (int i = 1; i <= numberOfTests; i++) {
@@ -120,10 +147,11 @@ public class Sending {
 					}
 					Request test;
 					test = Request.newPost();
-					test.setURI("coap://" + uri + ":" + testport + "/test");
+					String testURI = "coap://" + uri + ":" + testport + "/test";
+					test.setURI(testURI);
 					test.setType(type);
 					test.setPayload(DummyGenerator.makeDummydata(random.nextLong(), payloadSize));
-					test.send();
+					test.send(endpoint);
 					lastSentMessage = SystemClock.elapsedRealtime();
 					/*if (test.isConfirmable())
 						while (!test.isAcknowledged() && !test.isTimeouted() && !test.isCanceled() && !test.isRejected())
@@ -134,7 +162,7 @@ public class Sending {
 			}
 		} catch (InterruptedException e) {;}
 	}
-	private static void runMessageTest(Integer maxMessages, String uri, Integer testport, CoAP.Type type, Integer payloadSize,
+	private static void runMessageTest(CoAPEndpoint endpoint, String uri, Integer testport, Integer maxMessages, CoAP.Type type, Integer payloadSize,
 			Integer timeBetweenPackets, Integer timeBetweenTests, Integer numberOfTests) {
 		try {
 			for (int i = 1; i <= numberOfTests; i++) {
@@ -146,17 +174,18 @@ public class Sending {
 					}
 					Request test;
 					test = Request.newPost();
-					test.setURI("coap://" + uri + ":" + testport + "/test");
+					String testURI = "coap://" + uri + ":" + testport + "/test";
+					test.setURI(testURI);
 					test.setType(type);
 					test.setPayload(DummyGenerator.makeDummydata(random.nextLong(), payloadSize));
 //					if ((sentMessages+1) < 10 || (maxMessages-sentMessages) < 10)
 //						Log.d("dummycoap", "payloadstring" + test.getPayloadString().substring(0, 50).replaceAll("[\u0000-\u001f]", ""));
-					test.send();
+					test.send(endpoint);
 					sentMessages += 1;
 					lastSentMessage = SystemClock.elapsedRealtime();
-					/*if (test.isConfirmable())
+					if (test.isConfirmable())
 						while (!test.isAcknowledged() && !test.isTimeouted() && !test.isCanceled() && !test.isRejected())
-							Thread.sleep(1);*/
+							Thread.sleep(1);
 				}
 	    		if (i < numberOfTests)
 					Thread.sleep(timeBetweenTests);
